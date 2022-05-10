13dfcde35cb417c0bd54ce950e412178382c91da
xjxliuxing <xjxliuxing@163.com>
2022-05-11 02:12:53 +0800

    day04-start

---
+++ code/第五部分：Vue2/day04/01-vue/src/components/Right.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="right-container">
+    <h3>Right 组件</h3>
+    <hr />
+    <MyCount :init="6"></MyCount>
+  </div>
+</template><script>
+import MyCount from "@/components/Count.vue";
+
+export default {
+  components: {
+    MyCount: MyCount,
+  },
+};
+</script><style lang="less">
+.right-container {
+  padding: 0 20px 20px;
+  background-color: lightskyblue;
+  min-height: 250px;
+  flex: 1;
+}
+</style>
