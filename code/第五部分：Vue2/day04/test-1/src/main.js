2442fe38cd2311599216f5643ed5c7bbaef0cab2
xjxliuxing <xjxliuxing@163.com>
2022-05-18 01:47:16 +0800

    --全局组件

--- code/第五部分：Vue2/day04/test-1/src/main.js
+++ code/第五部分：Vue2/day04/test-1/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 // 1:在Main.js中引入
 import CommonVue from './components/CommonVue.vue'
 
+// 2:声明组件
 Vue.component('CommonVue', CommonVue)
 
 Vue.config.productionTip = false
