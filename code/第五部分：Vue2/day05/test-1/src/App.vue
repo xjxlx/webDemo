cc97ff040ddbceccc804e0ba2ee622cfe69dc8b4
xjxliuxing <xjxliuxing@163.com>
2022-05-23 02:09:30 +0800

    ref

---
+++ code/第五部分：Vue2/day05/test-1/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+
+		<!-- 1:给自定义组件设置ref的id -->
+		<Test1 ref="test"></Test1>
+
+		<button @click="leftBtn">获取Left的组件</button>
+	</div>
+</template>
+
+<script>
+	import Test1 from "@/components/Test1.vue"
+
+	export default {
+		name: 'App',
+		components: {
+			Test1
+		},
+		methods: {
+			leftBtn() {
+				// 2:使用test组件去控制变色
+				this.$refs.test.btn()
+			}
+		},
+	}
+</script>
+
+<style lang="less">
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
---
+++ code/第五部分：Vue2/day05/test-1/src/components/Test1.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="container">
+		<!-- 1：给组件设置ref的id -->
+		<p ref="a_container" class="">我是自定义的Test1</p>
+		<button @click="btn">点击改变p标签的内容</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Test1Test1',
+
+		methods: {
+			btn() {
+				// 2：使用指定id的ref组件
+				this.$refs.a_container.style.backgroundColor = "skyblue"
+			}
+		},
+
+	};
+</script>
+
+<style lang="less" scoped>
+	.container {
+		width: 50%;
+		background-color: pink;
+	}
+</style>
